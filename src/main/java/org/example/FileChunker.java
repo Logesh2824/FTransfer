022ed0c6ffa3c654b59a2b797fe5be8d7981c492
Logesh2824 <logeshms2824@gmail.com>
2026-05-05 17:11:40 +0530

    File transfer check

---
+++ src/main/java/org/example/FileChunker.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class FileChunker {
+    private static final int CHUNK_SIZE = 1024 * 1024;
+    public static void main(String[] args) {
+        // We will read from a test file and write to a copy
+        String sourceFile = "D:\\maven\\FTransfer\\src\\main\\java\\org\\example\\test.mp4";
+        String destinationFile = "testop.mp4";
+
+        copyFile(sourceFile, destinationFile);
+        System.out.println("File successfully chunked and reassembled!");
+    }
+
+    private static void copyFile(String sourcepath, String destpath) {
+        try (FileInputStream fis = new FileInputStream(sourcepath);
+             FileOutputStream fos = new FileOutputStream(destpath);
+             FileChannel inputchannel = fis.getChannel();
+             FileChannel outputchannel = fos.getChannel();) {
+            ByteBuffer buffer = ByteBuffer.allocate(CHUNK_SIZE);
+            while (inputchannel.read(buffer) > 0) {
+                buffer.flip();
+                outputchannel.write(buffer);
+                buffer.clear();
+            }
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
